fd1ca9e3ac9e7fb462ad9b5d1f0f027d63b9311d
Lancelot <haitong1110@126.com>
2020-10-30 16:19:39 +0800

    feat(Test): add a new Queue variable

--- src/io/kitejencien/test/Test.java
+++ src/io/kitejencien/test/Test.java
@@ -1,7 +1,11 @@
 package io.kitejencien.test;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Queue<String> strs = new LinkedList<>();
+        strs.offer("hi");
     }
 }
